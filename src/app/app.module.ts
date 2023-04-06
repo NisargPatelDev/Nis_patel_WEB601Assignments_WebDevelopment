13e1f6cb1f3f85c22989a89c7a6fb1f5780a2281
Nisarg Patel <nisargpateldev@gmail.com>
2023-04-06 13:15:00 -0400

    assignment 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { CreateContentComponent } from './create-content/create-content.component';
+import { AppMessagesComponent } from './app-messages/app-messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentCardComponent,
     ContentListComponent,
-    CreateContentComponent
+    CreateContentComponent,
+    AppMessagesComponent
   ],
   imports: [
     BrowserModule,
