c453626240cdf289eb163a58a3ba87f74e1aa074
Nisarg Patel <nisargpateldev@gmail.com>
2023-04-06 09:55:17 -0400

    assignment3

--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -1,4 +1,6 @@
+import { ViewportScroller } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { Content } from '../../../src/helper-files/content-interface';
 
 @Component({
@@ -8,6 +10,7 @@ import { Content } from '../../../src/helper-files/content-interface';
 })
 export class ContentListComponent {
   contentListArr:Content[] = [];
+  displayMsgCode:number=-2;
   generateContent(id:number,contentArr:(string)[]){
     return {
       id,
@@ -17,47 +20,74 @@ export class ContentListComponent {
     }
   }
 
-  constructor(){
+  userTitleInputForm = this.formBuilder.group({
+    contentTitleField: ''
+  });
+
+  onSubmit=()=>{
+    if (!this.userTitleInputForm.controls.contentTitleField.value) {
+      return;
+    }
+    this.displayMsgCode = this.contentListArr.findIndex(content=>content.title.toLowerCase()===this.userTitleInputForm.controls.contentTitleField.value?.toLowerCase());
+    if(this.displayMsgCode>-1){
+      this.contentListArr[this.displayMsgCode].highlight = true;   
+      this.scroller.scrollToAnchor(this.contentListArr[this.displayMsgCode].title);   
+    }
+    setTimeout(() => {
+      this.contentListArr[this.displayMsgCode].highlight = false;
+      this.displayMsgCode = -2;
+    }, 5000);
+  }
+
+  constructor(private formBuilder: FormBuilder, private scroller: ViewportScroller){
     this.contentListArr.push({
       ...this.generateContent(
-        2,['HTML','The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser. It is often assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript.','WHATWG']
+        1,['Avatar','Most popular aminated movie of all time','James Cameron']
       ),
-      ...{imgURL:'../assets/HTML.png'},
+      ...{imgURL:'../assets/avatar.jpg'},
       ...{type:'Movie'},
-      ...{tags:['Basic','Most Popular','Browser']}
+      ...{tags:['Animated','Most Popular','Creative']}
     });
     this.contentListArr.push({
       ...this.generateContent(
-        3,['HTML','The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser. It is often assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript.','WHATWG']
+        2,['Friends',`Friends is a 90's Comedy TV show`,'David Crane and Marta Kauffman']
       ),
-      ...{imgURL:'../assets/HTML.png'},
-      ...{type:'Movie'},
-      ...{tags:['Basic','Most Popular','Browser']}
+      ...{type:'Sitcom'},
+      ...{imgURL:'https://cdn.wallpapersafari.com/83/31/b59cLE.jpg'}
     });
     this.contentListArr.push({
       ...this.generateContent(
-        4,['HTML','The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser. It is often assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript.','WHATWG']
+        3,['Squid Game','Players in deep financial hardship, risk their lives to play a series of deadly Game','Hwang Dong-hyuk']
       ),
-      ...{imgURL:'../assets/HTML.png'},
-      ...{type:'Movie'},
-      ...{tags:['Basic','Most Popular','Browser']}
+      ...{type:'Web-Series'}
     });
     this.contentListArr.push({
       ...this.generateContent(
-        5,['HTML','The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser. It is often assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript.','WHATWG']
+        4,['3 Idiots',' Indian Hindi-language coming-of-age comedy-drama film written, edited and directed by Rajkumar Hirani','Rajkumar Hirani']
       ),
-      ...{imgURL:'../assets/HTML.png'},
       ...{type:'Movie'},
-      ...{tags:['Basic','Most Popular','Browser']}
+      ...{imgURL:'https://i.zoomtventertainment.com/story/Untitled_design_-_2020-05-01T135137.466.jpg'},
+      ...{tags:['Bollywood','Most Popular Hindispeaking Movie','Comedy','Based on book']}
     });
     this.contentListArr.push({
       ...this.generateContent(
-        6,['HTML','The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser. It is often assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript.','WHATWG']
-      ),
-      ...{imgURL:'../assets/HTML.png'},
-      ...{type:'Movie'},
-      ...{tags:['Basic','Most Popular','Browser']}
+        5,['A Beautiful Mind','A Beautiful Mind is a 2001 American biographical drama film directed by Ron Howard','Akiva Goldsman']
+      )
     });
-    
+    this.contentListArr.push({
+      ...this.generateContent(
+        6,['The Recruit','The Recruit is a 2003 American spy thriller film','Roger Donaldson']
+      )
+    });
+    this.contentListArr.push({
+      ...this.generateContent(
+        7,['The Shawshank Redemption','It is a 1994 American drama film','Frank Darabont']
+      )
+    });
+    this.contentListArr.push({
+      ...this.generateContent(
+        8,['The God Father','The Godfather is a 1972 American crime film','Francis Ford Coppola']
+      )
+    });
   }
 }
